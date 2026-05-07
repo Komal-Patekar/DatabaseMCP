7313a8ed1876bda4c928c6b55a3c72902ad30df6
Komal-Patekar <komal_patekar@vegayan.com>
2026-05-07 17:42:33 +0530

    Update main.py
    schema tracker listner added

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from ratelimiter import check_rate_limit
 from db import execute_procedure_call, run_query
 from discovery_tools import register_discovery_tools
 from tool_generator import register_database_tools
+from binlog_schema_tracker import start_listener
 
 
-mcp = FastMCP("Database_MCP_Server", auth=None)
+mcp = FastMCP("Database_MCP_Server")
 
 
 # register system discovery tools
@@ -25,6 +26,7 @@ def call_procedure(procedure_name: str, parameters: list = None):
     return execute_procedure_call(procedure_name, parameters or [])
 
 mcp.tool()(run_query)
+start_listener()
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="127.0.0.1", port=8080)
+    mcp.run()
